19c4e8e6874727c1c302fbe6f4ba2d52facc0fe9
Zhongyi-Lu <zhongyi_lu@outlook.com>
2023-02-16 17:39:37 -0800

    feat: add retweet bar

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faCircleCheck, faRetweet} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCircleCheck, faRetweet)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <App/>
     </React.StrictMode>
 );
 
