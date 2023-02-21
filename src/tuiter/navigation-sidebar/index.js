0be516658e531f4c8608eebfcd3aa1548113c4ef
Zhongyi-Lu <zhongyi_lu@outlook.com>
2023-02-21 15:15:38 -0800

    feat: add icons in navigation panel

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,47 +1,17 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import {useLocation} from "react-router";
+import items from "./navigationItems.js"
+import getNavigationItem from "./getNavigationItem.js";
 
+// TODO: refract this to components
 const NavigationSidebar = () => {
     const {pathname} = useLocation();
     const paths = pathname.split('/')
     const active = paths[2];
     return (
         <div className="list-group">
-            <a className="list-group-item" href="../../">Tuiter</a>
-            <Link to="/tuiter/tuiterHome" className={`list-group-item ${active === 'tuiterHome' ? 'active' : ''}`}>
-                Home
-            </Link>
-            <Link to="/tuiter/exploreScreen" className={`list-group-item ${active === 'exploreScreen' ? 'active' : ''}`}>
-                Explore
-            </Link>
-            <Link to="/" className="list-group-item">
-                Labs
-            </Link>
-            <a href="#/" className={`list-group-item
-                    ${active === 'notifications' ? 'active' : ''}`}>
-                Notifications
-            </a>
-            <a href="#/" className={`list-group-item
-                    ${active === 'messages' ? 'active' : ''}`}>
-                Messages
-            </a>
-            <a href="#/" className={`list-group-item
-                    ${active === 'bookmarks' ? 'active' : ''}`}>
-                Bookmarks
-            </a>
-            <a href="#/" className={`list-group-item
-                    ${active === 'lists' ? 'active' : ''}`}>
-                Lists
-            </a>
-            <a href="#/" className={`list-group-item
-                    ${active === 'profile' ? 'active' : ''}`}>
-                Profile
-            </a>
-            <a href="#/" className={`list-group-item
-                    ${active === 'more' ? 'active' : ''}`}>
-                More
-            </a>
+            {items.map(item => getNavigationItem({item, isActive: active === item.hrefName}))}
         </div>
     );
 };
