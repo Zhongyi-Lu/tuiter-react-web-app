591bf3c2656d16f02e61d6d3650d03fdd78f8e7f
Zhongyi-Lu <zhongyi_lu@outlook.com>
2023-02-21 17:07:37 -0800

    feat: finish 3.3.1

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {
+    faArrowUpFromBracket,
     faBell, faBookmark,
-    faCircleCheck, faEllipsis,
+    faCircleCheck, faComment, faEllipsis,
     faEnvelope, faFlask,
-    faHashtag,
+    faHashtag, faHeart,
     faHouseChimney, faList,
     faRetweet, faUser
 } from "@fortawesome/free-solid-svg-icons";
+import {faComment as faComment2, faHeart as faHeart2} from "@fortawesome/free-regular-svg-icons"
 import {faTwitter} from "@fortawesome/free-brands-svg-icons";
 
-library.add(faCircleCheck, faRetweet, faHouseChimney, faTwitter, faHashtag, faBell, faEnvelope, faBookmark, faList, faUser, faEllipsis, faFlask)
+library.add(faCircleCheck, faRetweet, faHouseChimney, faTwitter, faHashtag, faBell, faEnvelope, faBookmark, faList, faUser, faEllipsis, faFlask, faComment, faComment2, faHeart, faHeart2, faArrowUpFromBracket)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/tuiter/tuits/TuitItem.js
+++ src/tuiter/tuits/TuitItem.js
@@ -146,59 +146,33 @@ const TuitItem = ({item}) => {
 
                 <div className="wd-home-counter-container">
                     <div className="wd-home-counter-box">
-                        <svg fill="rgb(111, 120, 128)" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                             className="bi bi-chat" viewBox="0 0 16 16">
-                            <path
-                                d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
-                        </svg>
+                        <button className="wd-home-tweet-icon-button">
+                            <FontAwesomeIcon icon="fa-regular fa-comment"/>
+                        </button>
                         <div className="wd-home-counter-text">
                             {item.replies}
                         </div>
                     </div>
 
-                    <div className="wd-home-counter-box">
-                        <svg width="20px" height="20px" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg"
-                             strokeWidth="3" stroke="rgb(111, 120, 128)" fill="none">
-                            <path d="M52.94,42.93V18.3a5.54,5.54,0,0,0-5.54-5.54H11.83"/>
-                            <path d="M11.83,20.14V44.77a5.54,5.54,0,0,0,5.54,5.54H52.94"/>
-                            <polyline points="4.15 26.39 12.09 20.14 19.51 26.88"/>
-                            <polyline points="60.36 36.12 52.91 42.94 45 36.76"/>
-                        </svg>
-
+                    <div className="wd-home-counter-box" style={{color: item.retuited ? '#00b97f' : 'inherent'}}>
+                        <FontAwesomeIcon icon="fa-solid fa-retweet"/>
                         <div className="wd-home-counter-text">
                             {item.retuits}
                         </div>
                     </div>
 
-                    <div className="wd-home-counter-box">
-                        <svg fill="#6f787f" height="16px" width="16px" version="1.1" id="Capa_1"
-                             xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
-                             viewBox="0 0 471.701 471.701" xmlSpace="preserve">
-                            <g>
-                                <path d="M433.601,67.001c-24.7-24.7-57.4-38.2-92.3-38.2s-67.7,13.6-92.4,38.3l-12.9,12.9l-13.1-13.1
-                                    c-24.7-24.7-57.6-38.4-92.5-38.4c-34.8,0-67.6,13.6-92.2,38.2c-24.7,24.7-38.3,57.5-38.2,92.4c0,34.9,13.7,67.6,38.4,92.3
-                                    l187.8,187.8c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-3.9l188.2-187.5c24.7-24.7,38.3-57.5,38.3-92.4
-                                    C471.801,124.501,458.301,91.701,433.601,67.001z M414.401,232.701l-178.7,178l-178.3-178.3c-19.6-19.6-30.4-45.6-30.4-73.3
-                                    s10.7-53.7,30.3-73.2c19.5-19.5,45.5-30.3,73.1-30.3c27.7,0,53.8,10.8,73.4,30.4l22.6,22.6c5.3,5.3,13.8,5.3,19.1,0l22.4-22.4
-                                    c19.6-19.6,45.7-30.4,73.3-30.4c27.6,0,53.6,10.8,73.2,30.3c19.6,19.6,30.3,45.6,30.3,73.3
-                                    C444.801,187.101,434.001,213.101,414.401,232.701z"/>
-                            </g>
-                        </svg>
-
-                        <i className="fa-regular fa-heart"></i>
+                    <div className="wd-home-counter-box" style={{color: item.liked ? '#fc217f' : 'inherent'}}>
+                        {item.liked ?
+                            <FontAwesomeIcon icon="fa-solid fa-heart"/>
+                            : <FontAwesomeIcon icon="fa-regular fa-heart"/>
+                        }
                         <div className="wd-home-counter-text">
                             {item.likes}
                         </div>
                     </div>
 
                     <div className="wd-home-counter-box">
-                        <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M15 6L12 3M12 3L9 6M12 3V15M16 10H17C18.1046 10 19 10.8954 19 12V18C19 19.1046 18.1046 20 17 20H7C5.89543 20 5 19.1046 5 18V12C5 10.8954 5.89543 10 7 10H8"
-                                stroke="#6f787f" strokeWidth="1" strokeLinecap="round"
-                                strokeLinejoin="round"/>
-                        </svg>
+                        <FontAwesomeIcon icon="fa-solid fa-arrow-up-from-bracket" />
                     </div>
 
                 </div>
