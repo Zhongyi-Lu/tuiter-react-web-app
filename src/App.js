f5d3f0b3832fafc41011292eece121123b6eeb40
Zhongyi-Lu <98251609+Zhongyi-Lu@users.noreply.github.com>
2023-03-20 18:05:35 -0700

    feat: use `.env`

--- src/App.js
+++ src/App.js
@@ -5,21 +5,22 @@ import Tuiter from "./tuiter/index.js";
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
 
+
 function App() {
-    return (
-        <BrowserRouter>
-            <div className="container">
-                <Routes>
-                    <Route path="/*"
-                           element={<Labs/>}/>
-                    <Route path="/hello"
-                           element={<HelloWorld/>}/>
-                    <Route path="/tuiter/*"
-                           element={<Tuiter/>}/>
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/*"
+                 element={<Labs/>}/>
+          <Route path="/hello"
+                 element={<HelloWorld/>}/>
+          <Route path="/tuiter/*"
+                 element={<Tuiter/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
